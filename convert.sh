cf9ce40ea300f93c25d21fcea3039d9610af8140
root <root@bestgame-17.(none)>
2018-08-25 11:08:49 +0800

    [o][iso 转成  utf8]

--- convert.sh
+++ convert.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 mkdir -p log
+file_list=`find ../../protocol/ -name *.proto -type f|xargs file -i|grep -v -E "utf-8|ascii"|awk -F ":" '{print $1}'`
+for v in ${file_list}
+do
+	iconv -f iso-8859-1 -t utf8 ${v} -o ${v}
+done
 ./convert_data.sh
 ./convert_pb.sh
+for v in ${file_list}
+do
+	iconv -f utf8 -t iso-8859-1 ${v} -o ${v}
+done
